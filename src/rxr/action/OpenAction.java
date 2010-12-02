1ac82d724ae365b6782db1e1cb35ee90c225a44e
Josh Vinson <zyxwyvu@gmail.com>
2010-12-02 10:51:21 -0800

    Open dialog now allows selections that aren't .txt files Changed version to 0.7 Minor edit to build.xml

--- src/rxr/action/OpenAction.java
+++ src/rxr/action/OpenAction.java
@@ -33,8 +33,8 @@ public class OpenAction extends AbstractAction
 	{
 		boolean done = false;
 		JFileChooser fc = new JFileChooser();
-		fc.addChoosableFileFilter(new TextFilter());
-		fc.setAcceptAllFileFilterUsed(false);
+		//fc.addChoosableFileFilter(new TextFilter());
+		//fc.setAcceptAllFileFilterUsed(false);
 		while(!done)
 		{
 			int returnVal = fc.showOpenDialog(RXR.window);
